277d687c9ec57c7f6a4bd0f8ab2f764fc338bb83
whoisleigh <122069076+whoisleigh@users.noreply.github.com>
2023-01-22 21:30:02 -0600

    Update playercam.cs

--- playercam.cs
+++ playercam.cs
@@ -16,20 +16,20 @@ public class playercam : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.visible = false;  //makes it so the cursor stays in centre of screen/doesn't move and also not seen
     }
 
     private void Update()
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.smoothDeltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.smoothDeltaTime * sensY;
-
+//the above is getting the mouse input
         yRotation += mouseX;
-
+//handling rotation
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);//making sure you cant look up or down too far
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);//rotating camera and orientation
     }
 }
